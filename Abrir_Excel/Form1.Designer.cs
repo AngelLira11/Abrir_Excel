3de306b9055119e6f2e0ec7a54543095ad318e84
AngelLira11 <prodigylira777@gmail.com>
2025-02-17 22:41:11 -0600

    Agregar archivos de proyecto.

---
+++ Abrir_Excel/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Abrir_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            excelToolStripMenuItem = new ToolStripMenuItem();
+            guardarArchivoToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialogEditor = new OpenFileDialog();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            dataGridViewEditor = new DataGridView();
+            btnColumna = new Button();
+            btnFila = new Button();
+            txtNameColumna = new TextBox();
+            label1 = new Label();
+            menuStrip1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewEditor).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { excelToolStripMenuItem, guardarArchivoToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(73, 24);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // excelToolStripMenuItem
+            // 
+            excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            excelToolStripMenuItem.Size = new Size(217, 26);
+            excelToolStripMenuItem.Text = "Abrir Archivo ( .xls)";
+            excelToolStripMenuItem.Click += excelToolStripMenuItem_Click;
+            // 
+            // guardarArchivoToolStripMenuItem
+            // 
+            guardarArchivoToolStripMenuItem.Name = "guardarArchivoToolStripMenuItem";
+            guardarArchivoToolStripMenuItem.Size = new Size(217, 26);
+            guardarArchivoToolStripMenuItem.Text = "Guardar Archivo";
+            // 
+            // openFileDialogEditor
+            // 
+            openFileDialogEditor.FileName = "openFileDialog1";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(dataGridViewEditor, 0, 0);
+            tableLayoutPanel1.Location = new Point(0, 31);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 84.76659F));
+            tableLayoutPanel1.Size = new Size(800, 341);
+            tableLayoutPanel1.TabIndex = 1;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // dataGridViewEditor
+            // 
+            dataGridViewEditor.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewEditor.Dock = DockStyle.Fill;
+            dataGridViewEditor.Location = new Point(3, 3);
+            dataGridViewEditor.Name = "dataGridViewEditor";
+            dataGridViewEditor.RowHeadersWidth = 51;
+            dataGridViewEditor.Size = new Size(794, 335);
+            dataGridViewEditor.TabIndex = 0;
+            // 
+            // btnColumna
+            // 
+            btnColumna.Location = new Point(49, 395);
+            btnColumna.Name = "btnColumna";
+            btnColumna.Size = new Size(148, 29);
+            btnColumna.TabIndex = 2;
+            btnColumna.Text = "Agregar Columna";
+            btnColumna.UseVisualStyleBackColor = true;
+            btnColumna.Click += btnColumna_Click;
+            // 
+            // btnFila
+            // 
+            btnFila.Location = new Point(203, 395);
+            btnFila.Name = "btnFila";
+            btnFila.Size = new Size(128, 29);
+            btnFila.TabIndex = 3;
+            btnFila.Text = "Agregar Fila";
+            btnFila.UseVisualStyleBackColor = true;
+            btnFila.Click += btnFila_Click;
+            // 
+            // txtNameColumna
+            // 
+            txtNameColumna.Location = new Point(553, 397);
+            txtNameColumna.Name = "txtNameColumna";
+            txtNameColumna.Size = new Size(125, 27);
+            txtNameColumna.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(415, 400);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Nombre columna: ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(txtNameColumna);
+            Controls.Add(btnFila);
+            Controls.Add(btnColumna);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewEditor).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem excelToolStripMenuItem;
+        private OpenFileDialog openFileDialogEditor;
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dataGridViewEditor;
+        private ToolStripMenuItem guardarArchivoToolStripMenuItem;
+        private Button btnColumna;
+        private Button btnFila;
+        private TextBox txtNameColumna;
+        private Label label1;
+    }
+}
---
+++ Abrir_Excel/Form1.cs
@@ -0,0 +1,109 @@
+using System.Windows.Forms;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Abrir_Excel
+{
+    public partial class Form1 : Form
+    {
+        string nombre_heder = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialogo = new OpenFileDialog { Filter = "Excel Files|*.xls;*.xlsx", Title = "Selecciona un archivo de Excel" };
+            if (dialogo.ShowDialog() == DialogResult.OK) CargarExcel(dialogo.FileName);
+
+
+        }
+        //se utilizo la libreria Oledb para abrir archivos en excel la cual permite ver el archivo 
+        //en el datagridview talk y cual se mostraria en el archivo de excel 
+
+        //FUENTES: https://learn.microsoft.com/es-es/office/vba/excel/concepts/ole-db-for-olap-properties-used-by-excel
+        private void CargarExcel(string ruta)
+        {
+            string aux = ruta.EndsWith(".xls") ?
+           $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={ruta};Extended Properties='Excel 8.0;HDR=YES;IMEX=1;'" :
+           $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={ruta};Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
+
+            try
+            {
+                using (var conn = new OleDbConnection(aux))
+                {
+                    conn.Open();
+                    string hoja = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[0]["TABLE_NAME"].ToString();
+                    var adaptador = new OleDbDataAdapter($"SELECT * FROM [{hoja}]", conn);
+                    var dt = new DataTable();
+                    adaptador.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        dataGridViewEditor.Columns.Clear();
+                        dataGridViewEditor.DataSource = dt; 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnColumna_Click(object sender, EventArgs e)
+        {
+            string name = txtNameColumna.Text;
+
+            if (dataGridViewEditor.Columns.Count == 0)
+            {
+                //agrega la primera columna si esta vacia la tabla
+                dataGridViewEditor.Columns.Add(name, name);
+            }
+            else
+            {
+                // agregar nueva columna a la tabla
+                int nuevaColumnaIndex = dataGridViewEditor.Columns.Count + 1;
+
+                // poner nombre a la columna
+                string nombreColumna = name + nuevaColumnaIndex;
+                string encabezadoColumna = name + nuevaColumnaIndex;
+
+                // agregar columna con nombre
+                dataGridViewEditor.Columns.Add(nombreColumna, encabezadoColumna);
+            }
+        }
+
+        private void btnFila_Click(object sender, EventArgs e)
+        {
+            
+            if (dataGridViewEditor.Columns.Count == 0)
+            {
+                MessageBox.Show("Debe haber al menos una columna antes de agregar filas.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Crear una nueva fila vacía
+            DataGridViewRow nuevaFila = new DataGridViewRow();
+
+            // Agregar celdas vacías a la nueva fila
+            for (int i = 0; i < dataGridViewEditor.Columns.Count; i++)
+            {
+                nuevaFila.Cells.Add(new DataGridViewTextBoxCell());
+            }
+
+            // Agregar nueva fila a la tabla
+            dataGridViewEditor.Rows.Add(nuevaFila);
+        }
+    }
+}
